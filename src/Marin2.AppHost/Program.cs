e2d35420976c48f92b14a728f405adf8d5ccde84
Oscar Marin <oscar@marin.cr>
2024-12-23 00:40:47 -0600

    Update Program.cs

--- src/Marin2.AppHost/Program.cs
+++ src/Marin2.AppHost/Program.cs
@@ -4,8 +4,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var servApp = builder.AddProject<Projects.Luval_Marin2_Services>("marin2-services");
 
 var uiApp = builder.AddProject<Projects.Luval_Marin2_UI>("marin2-ui")
+    .WithExternalHttpEndpoints()
     .WithReference(servApp);
-
 
 
 builder.Build().Run();
