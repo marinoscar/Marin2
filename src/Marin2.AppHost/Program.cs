15506830bbeb16dc8b01d535f8bf2fab853a864a
Oscar Marin <oscar@marin.cr>
2024-12-23 00:04:56 -0600

    deployment updates

--- src/Marin2.AppHost/Program.cs
+++ src/Marin2.AppHost/Program.cs
@@ -1,9 +1,9 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var servApp = builder.AddProject<Projects.Luval_Marin2_Services>("Marin2-Services");
+var servApp = builder.AddProject<Projects.Luval_Marin2_Services>("marin2-services");
 
-var uiApp = builder.AddProject<Projects.Luval_Marin2_UI>("Marin2-UI")
+var uiApp = builder.AddProject<Projects.Luval_Marin2_UI>("marin2-ui")
     .WithReference(servApp);
 
 
