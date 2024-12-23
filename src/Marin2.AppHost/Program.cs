e15429295175a36362f3f1929ce82c3543bad7e2
Oscar Marin <oscar@marin.cr>
2024-12-23 09:29:31 -0600

    Update Program.cs
    added reference to postgres and created a database

--- src/Marin2.AppHost/Program.cs
+++ src/Marin2.AppHost/Program.cs
@@ -1,11 +1,19 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+
+// Add postgres instance with pgAdmin
+var postgres = builder.AddPostgres("db")
+    .WithPgAdmin();
+
+var authMateDb = postgres.AddDatabase("authmate");
+
 var servApp = builder.AddProject<Projects.Luval_Marin2_Services>("marin2-services");
 
 var uiApp = builder.AddProject<Projects.Luval_Marin2_UI>("marin2-ui")
     .WithExternalHttpEndpoints()
-    .WithReference(servApp);
+    .WithReference(servApp)
+    .WithReference(authMateDb);
 
 
 builder.Build().Run();
