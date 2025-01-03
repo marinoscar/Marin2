f7bd5e8f45c10713a7d3af517cee9a5122dc8d87
Oscar Marin <oscar@marin.cr>
2025-01-02 21:34:00 -0600

    code clean up

--- src/Luval.Marin2.UI/Program.cs
+++ src/Luval.Marin2.UI/Program.cs
@@ -4,7 +4,6 @@ using Luval.AuthMate.Core.Resolver;
 using Luval.AuthMate.Infrastructure.Configuration;
 using Luval.AuthMate.Infrastructure.Data;
 using Luval.AuthMate.Postgres;
-using Luval.Marin2.Infrastructure.Configuration;
 using Luval.Marin2.UI.Components;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.FluentUI.AspNetCore.Components;
@@ -17,6 +16,8 @@ namespace Luval.Marin2.UI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var config = builder.Configuration;
+
             builder.AddServiceDefaults();
 
             // Add services to the container.
@@ -30,6 +31,9 @@ namespace Luval.Marin2.UI
             builder.Services.AddHttpContextAccessor();
 
             //registers all of the services needed for AuthMate
+            var connManager = new OAuthConnectionManager(config);
+            var googleConfig = connManager.GetConfiguration("Google");
+
             var connStr = builder.Configuration.GetConnectionString("marin2");
             var key = Environment.GetEnvironmentVariable("authmate-bearingtokenkey");
             builder.Services.AddAuthMateServices(key ?? string.Empty,
@@ -39,14 +43,13 @@ namespace Luval.Marin2.UI
                     return new PostgresAuthMateContext(connStr ?? string.Empty);
                 });
 
-            var options = builder.Configuration.GetOAuthProvider("Google");
-            if(options == null || string.IsNullOrEmpty(options.ClientSecret)) throw new Exception("Google OAuth configuration not found");
+            
             builder.Services.AddAuthMateGoogleAuth(new GoogleOAuthConfiguration()
             {
                 // client id from your config file
-                ClientId = options.ClientId,
+                ClientId = googleConfig.ClientId,
                 // the client secret from your config file
-                ClientSecret = options.ClientSecret,
+                ClientSecret = googleConfig.ClientSecret,
             });
 
             var app = builder.Build();
