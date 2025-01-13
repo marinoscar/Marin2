9f07284e969ebd1cb1e7b7bc8dfe28ea5124b08d
Oscar Marin <oscar@marin.cr>
2025-01-12 19:49:33 -0600

    Update Program.cs

--- src/Marin2.AppHost/Program.cs
+++ src/Marin2.AppHost/Program.cs
@@ -1,24 +1,25 @@
 
 using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Add secret for the Parameters:AzureAppConnString to the user screts to set the value
 var azureAppConfig = builder.AddParameter("AzureAppConnString", true);
 
-// Add postgres instance with pgAdmin
-var postgres = builder.AddPostgres("db")
-    .WithDataVolume()
-    .WithPgAdmin();
 
-var authMateDb = postgres.AddDatabase("marin2");
+var sql = builder.AddSqlServer("sql")
+                 .WithLifetime(ContainerLifetime.Persistent);
+
+var marin2Db = sql.AddDatabase("marin2");
 
 
 // Configure the UI project
 var uiApp = builder.AddProject<Projects.Luval_Marin2_UI>("marin2-ui")
     .WithExternalHttpEndpoints()
-    .WaitFor(postgres)
-    .WithReference(authMateDb)
+    .WaitFor(sql)
+    .WithReference(marin2Db)
     .WithEnvironment("AzureAppConnString", azureAppConfig);
 
 
