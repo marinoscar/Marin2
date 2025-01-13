1f3f13abbdbd793be87fc1929a98bbf771f96d13
Oscar Marin <oscar@marin.cr>
2025-01-12 19:53:49 -0600

    Update Program.cs

--- src/Marin2.AppHost/Program.cs
+++ src/Marin2.AppHost/Program.cs
@@ -10,7 +10,8 @@ var azureAppConfig = builder.AddParameter("AzureAppConnString", true);
 
 
 var sql = builder.AddSqlServer("sql")
-                 .WithLifetime(ContainerLifetime.Persistent);
+                 .WithLifetime(ContainerLifetime.Persistent)
+                 .WithDataVolume();
 
 var marin2Db = sql.AddDatabase("marin2");
 
